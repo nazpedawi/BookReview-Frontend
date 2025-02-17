eeb84acf3115c312f608dcae32acd3915da8fe1f
nazpedawi <144240165+nazpedawi@users.noreply.github.com>
2025-02-17 18:42:36 +0100

    added image filetypes

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  assetsInclude: ["**/*.PNG"],
+  assetsInclude: ["**/*.PNG", "**/*.jpeg", "**/*.jpg", "**/*.webp", "**/*.png"],
 });
