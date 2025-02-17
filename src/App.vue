e9600311453b84d8cace64736acc24a48b9e023a
nazpedawi <144240165+nazpedawi@users.noreply.github.com>
2025-02-17 18:39:53 +0100

    added navbar and footer

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,21 @@
-<template>  </template>
+<template>
+    <div class="app">
+        <Navbar />
+      <router-view /> <!-- This will load your pages -->
+      <Footer />
+    </div>
+  </template>
+  
+  <script setup>
+  import Footer from "@/components/Footer.vue";
+  import Navbar from "@/components/Navbar.vue";
+  </script>
+  
+  <style>
+  .app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+  </style>
+  
---
+++ src/components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+    <footer class="bg-dark text-white text-center py-3">
+      <p>&copy; 2024 Book Review. All rights reserved.</p>
+    </footer>
+  </template>
+  
